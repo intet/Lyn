ab4e70a492401da5fc7d6cc187c4c6309cf9bc1f
Сиводедов Дмитрий Александрович <sivodedov_da@prosoftlab.ru>
2018-05-15 15:28:43 +0300

    init user

---
+++ backend/security/src/main/java/com/intetm/security/rest/InitService.java
@@ -0,0 +1,7 @@
+package com.intetm.security.rest;
+
+import com.intetm.model.User;
+
+public interface InitService {
+    void initUser(User user);
+}
---
+++ backend/trainer/src/main/java/com/intetm/trainer/service/InitServiceImpl.java
@@ -0,0 +1,7 @@
+package com.intetm.trainer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InitServiceImpl {
+}
